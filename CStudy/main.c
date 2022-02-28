bb7f9a6fd65cf85e9cdc6f93f6d6e7a74780ab32
illinia <seolin1218@gmail.com>
2022-02-28 19:53:12 +0900

    Initial Commit

---
+++ CStudy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CStudy
+//
+//  Created by 김태민 on 28.02.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
